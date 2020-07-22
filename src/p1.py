cfa681dbdf0f05ba75adeba292514f6ca20ea842
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-22 14:05:54 +0200

    Adding second layer and batch input

--- src/p1.py
+++ src/p1.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
@@ -8,9 +10,14 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
-'''
-Need to put weights first as first input determines the shape of the output
-and we want the the results of each neuron
-'''
-print(output)
+weights2 = [[0.1, -0.14, 0.5],
+           [-0.5, 0.12, -0.33],
+           [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)

fde59f7bf163eaa3d097705c32f37b686fba6443
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-22 14:31:16 +0200

    Creating Layer class and forward pass method

--- src/p1.py
+++ src/p1.py
@@ -1,23 +1,24 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
 
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-biases2 = [-1, 2, -0.5]
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
+layer1.forward(X)
+# print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)

8eaec5213742a246786cc2d7166aa84061768755
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-22 12:06:40 +0200

    Added numpy usage
    Calculating output using numpy dot product method

--- src/p1.py
+++ src/p1.py
@@ -1,4 +1,4 @@
-# Basic layout of what a single layer of a neural network looks like
+import numpy as np
 
 inputs = [1, 2, 3, 2.5]
 
@@ -8,14 +8,9 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-layer_outputs = []
-
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
-
+output = np.dot(weights, inputs) + biases
+'''
+Need to put weights first as first input determines the shape of the output
+and we want the the results of each neuron
+'''
+print(output)
